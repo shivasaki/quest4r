44123f6d4c8b8316f0a6ac651cb1cc5ce0b0ea20
shivasaki <shivasaki0329@gmail.com>
2024-02-12 21:49:17 +0900

    update rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  root 'site#top'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#login'
-  delete 'logout'  => 'sessions#destroy'
-  get 'user/new'
-  post 'user/create'
-  get 'user/edit' #/:id
-  #putch 'user/:id'
-  #delete 'user/destroy/:id'
-  get 'top' => 'site#top'
-  get 'questions/index_col' => 'question#index_col'
-  get 'questions/index_hs' => 'question#index_hs'
-  get 'question/show'
-  get 'question/new'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
