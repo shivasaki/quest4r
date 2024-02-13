d1b6b554458a2b9f5ac97b666dc1175cca72fa3b
shivasaki <shivasaki0329@gmail.com>
2024-02-13 16:08:21 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+
+  get 'about', to: 'pages#about'
+
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :questions
+  resources :answers
 end
